3e010b3808a3f06c3737f8f6a1c6c9b260182cb0
Jim Hester <james.f.hester@gmail.com>
2014-12-22 10:54:53 -0500

    package coverage _almost_ working, just breaks on NSE

--- R/covr.R
+++ R/covr.R
@@ -1,10 +1,19 @@
 #' @import jsonlite
 
-count <- numeric()
+expression_coverage <- function(x, ..., env = parent.frame()) {
+  args <- lazyeval::lazy_dots(...)
+  expression_coverage_(x, args)
+}
+expression_coverage_ <- function(x, args, env = parent.frame()) {
 
-trace_expressions <- function (x, srcref = NULL, ...) {
+  exprs <- lazyeval::as.lazy_dots(args, env)
+  str(exprs)
+
+  `*counts*` <- list()
+
+  trace_expressions <- function (x, srcref = NULL) {
     recurse <- function(y) {
-      lapply(y, trace_expressions, ...)
+      lapply(y, trace_expressions)
     }
 
     if (is.atomic(x) || is.name(x)) {
@@ -13,18 +22,18 @@ trace_expressions <- function (x, srcref = NULL, ...) {
     else if (is.call(x)) {
       src_ref <- attr(x, "srcref")
       if (!is.null(src_ref)) {
-        message("type: ", typeof(x), " class: ", class(x), " length: ", length(src_ref))
+        #message("type: ", typeof(x), " class: ", class(x), " length: ", length(src_ref))
         as.call(Map(trace_expressions, x, src_ref))
       } else if (!is.null(srcref)) {
-        srcref[9] <- 0
-          bquote(`{`(test_print(.(srcref)), .(as.call(recurse(x)))))
+        `*counts*`[key(srcref)] <<- 0
+        bquote(`{`(`*trace*`(.(srcref)), .(as.call(recurse(x)))))
       } else {
         as.call(recurse(x))
       }
     }
     else if (is.function(x)) {
-      formals(x) <- trace_expressions(formals(x), ...)
-      body(x) <- trace_expressions(body(x), ...)
+      formals(x) <- trace_expressions(formals(x))
+      body(x) <- trace_expressions(body(x))
       x
     }
     else if (is.pairlist(x)) {
@@ -40,16 +49,31 @@ trace_expressions <- function (x, srcref = NULL, ...) {
       stop("Unknown language class: ", paste(class(x), collapse = "/"),
         call. = FALSE)
     }
+  }
+
+  `*trace*` <- function(y) {
+    key <- key(y)
+    `*counts*`[[key]] <<- `*counts*`[[key]] + 1
+  }
+
+  for (file_expressions in x) {
+    eval(trace_expressions(file_expressions$expr))
+  }
+
+  print(ls())
+  for (expr in exprs) {
+    lazyeval::lazy_eval(expr, environment())
+  }
+
+  `*counts*`
 }
 
-test_print <- function(x) {
-  if (is.na(count[x[1]])) {
-    count[x[1]] <<- 1
-  } else {
-    count[x[1]] <<- count[x[1]] + 1
-  }
+key <- function(x) {
+  file <- attr(x, "srcfile")$filename %||% "<default>"
+  paste(sep = ":", file, paste0(collapse = ":", c(x)))
 }
-expr <- function(filename) {
+
+get_srcfile <- function(filename) {
   lines <- readLines(filename)
   source_file <- srcfilecopy(filename, lines, file.info(filename)[1, "mtime"], isFile = TRUE)
   source_file$expr <- parse(text = lines, srcfile = source_file, keep.source = TRUE)
@@ -64,6 +88,18 @@ expr <- function(filename) {
   source_file$parsed <- getParseData(source_file)
 
   source_file
+}
+
+package_coverage <- function(path = ".") {
+  source_files <- dir(file.path(path, "R"), pattern = "\\.[Rr]$", full.names = TRUE)
+
+  test_files <- dir(file.path(path, "tests", "testthat"), recursive = TRUE, pattern = "\\.[Rr]$", full.names = TRUE)
+
+  source_parsed <- lapply(source_files, get_srcfile)
+
+  test_parsed <- lapply(test_files, function(x) parse(x))
+
+  expression_coverage_(source_parsed, test_parsed)
 }
 #jsonlite::toJSON(na="null",
   #list("service_job_id" = 123324,
