7df1f7ea41d6967b5a72b60cce35067f1ac6e2af
Jim Hester <james.f.hester@gmail.com>
2014-12-29 10:33:16 -0500

    Move coveralls to separate file.

---
+++ R/coveralls.R
@@ -0,0 +1,39 @@
+#' @export
+coveralls <- function(path = ".", ...) {
+  coveralls_url <- "https://coveralls.io/api/v1/jobs"
+  coverage <- to_coveralls(package_coverage(path, relative_path = TRUE, ...))
+
+  name <- tempfile()
+  con <- file(name)
+  writeChar(con = con, coverage, eos = NULL)
+  close(con)
+  httr::content(httr::POST(coveralls_url, body = list(json_file = httr::upload_file(name))))
+}
+
+to_coveralls <- function(x, service_job_id = Sys.getenv("TRAVIS_JOB_ID"), service_name = "travis-ci") {
+  coverages <- per_line(x)
+
+  names <- names(coverages)
+
+  sources <- lapply(names(coverages),
+    function(x) {
+      readChar(x, file.info(x)$size)
+    })
+
+  res <- mapply(
+    function(name, source, coverage) {
+      list("name" = jsonlite::unbox(name),
+        "source" = jsonlite::unbox(source),
+        "coverage" = coverage)
+    },
+    names,
+    sources,
+    coverages,
+    SIMPLIFY = FALSE,
+    USE.NAMES = FALSE)
+
+  jsonlite::toJSON(na = "null", list(
+    "service_job_id" = jsonlite::unbox(service_job_id),
+    "service_name" = jsonlite::unbox(service_name),
+    "source_files" = res))
+}
