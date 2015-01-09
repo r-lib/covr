9f89a4bda6cd761faa687c0903e179c3c8d1c51b
Jim Hester <james.f.hester@gmail.com>
2015-01-09 10:33:47 -0500

    Update Compiled tests to pass

--- tests/testthat/test-Compiled.R
+++ tests/testthat/test-Compiled.R
@@ -2,7 +2,7 @@ context("Compiled")
 test_that("Compiled code coverage is reported", {
   cov <- as.data.frame(package_coverage("TestCompiled", relative_path = TRUE))
 
-  expect_equal(cov$first_line, c(3, 20, 26, 32))
+  expect_equal(cov$first_line, c(3, 6, 7, 9, 11, 12, 14, 15, 16, 17, 18, 19, 20, 22, 24))
 
-  expect_equal(cov$value, c(5, 1, 1, 1))
+  expect_equal(cov$value, c(4, 4, 4, 4, 4, 4, 4, 3, 3, 1, 0, 0, 1, 4, 4))
 })
