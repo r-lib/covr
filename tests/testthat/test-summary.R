2e4b1631b527cb80e0ec3a3cf3022a8e9e7737b5
Robert Flight <r.flight79@gmail.com>
2015-01-09 09:21:08 -0500

    changed to using the new TestSummary package, and rearranged values to be in-line with @jimhester expect_ tests

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,11 +1,11 @@
 context("summary_functions")
 
-test_that("S4 gives 100% coverage", {
-  expect_equal(1, percent_coverage(package_coverage("TestS4")))
+test_that("Summary gives 50% coverage", {
+  expect_equal(percent_coverage(package_coverage("TestSummary")), 0.5)
 })
 
-zero_S4 <- zero_coverage(package_coverage("TestS4"))
+zero_Summary <- zero_coverage(package_coverage("TestSummary"))
 
-test_that("S4 gives 0 lines with 0 coverage", {
-  expect_equal(0, nrow(zero_S4))
+test_that("Summary gives 1 lines with 0 coverage", {
+  expect_equal(nrow(zero_Summary), 1)
 })
