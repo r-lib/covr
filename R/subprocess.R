2b5e5c9f7226842548433afdf80c7b121836ee59
Jim Hester <james.f.hester@gmail.com>
2015-01-15 15:59:16 -0500

    Execute subprocess quietly.

--- R/subprocess.R
+++ R/subprocess.R
@@ -1,8 +1,16 @@
-# this function calls a subprocess with expressions given in ..., trying to
-# replicate as much of the calling environment as possible, then returning the
-# results to the calling environment.
+#' Run R commands in a R subprocess
+#'
+#' this function runs a R subprocess with expressions given in ..., trying to
+#' replicate as much of the calling environment as possible, then returning the
+#' results to the calling environment.
+#' @param ... expressions to call in the subprocess
+#' @param calling_env the environment of the calling function.
+#' @param global_env the global environment to load in the subprocess.
+#' @param clean_output whether to clean the Rout files generated from the subprocess.
+#' @param quiet whether to echo the R command run.
 subprocess <- function(..., calling_env = parent.frame(),
                             global_env = .GlobalEnv,
+                            quiet = TRUE,
                             clean_output = TRUE) {
   exprs <- eval(substitute(alist(...)))
 
@@ -39,7 +47,11 @@ fun()",
 tmp_global_env, tmp_exprs, tmp_output, tmp_calling_env)
 
   writeChar(con = tmp_source, command, eos = NULL)
-  output <- try(devtools:::RCMD("BATCH", c("--vanilla", "--quiet", tmp_source), path = "."))
+  output <- try(devtools:::RCMD("BATCH",
+                                c("--slave", tmp_source),
+                                path = ".",
+                                quiet = TRUE),
+                              silent = quiet)
 
   if (inherits(output, "try-error")) {
     stop("Command Output: \n",
@@ -51,14 +63,14 @@ tmp_global_env, tmp_exprs, tmp_output, tmp_calling_env)
   if (clean_output) {
     if (file.exists(rout_file)) {
       unlink(rout_file)
-    } else {
-      message(rout_file, " does not exist")
+    } else if (!quiet) {
+        message(rout_file, " does not exist")
     }
   }
 
   if (file.exists(tmp_output)) {
     load(envir = calling_env, file = tmp_output)
-  } else {
-    message(tmp_output, " does not exist")
+  } else if (!quiet) {
+        message(tmp_output, " does not exist")
   }
 }
