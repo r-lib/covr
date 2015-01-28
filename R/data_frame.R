r-lib/covr

commit efbc295ad6f8e5eb051999440e55a4e83dd4cf7c

Add start and end column to zero_coverage
closes #42

R/data_frame.R
@@ -16,5 +16,7 @@ as.data.frame.coverage <- function(x, row.names = NULL, optional = FALSE, ...) {
 
   df[] <- lapply(df, type.convert, as.is = TRUE)
   df$value <- unlist(x)
-  df[order(df$filename, df$first_line, df$first_byte),]
+  df <- df[order(df$filename, df$first_line, df$first_byte),]
+  rownames(df) <- NULL
+  df
 }

R/summary_functions.R
@@ -24,13 +24,8 @@ percent_coverage <- function(coverage_result, by_line = TRUE) {
 zero_coverage <- function(coverage_result) {
   coverage_data <- as.data.frame(coverage_result)
 
-  zero_locs <- coverage_data$value == 0
-
-  coverage_zero <- coverage_data[zero_locs,
-    c("filename", "first_line", "last_line", "value")]
-  rownames(coverage_zero) <- NULL
-
-  coverage_zero
+  coverage_data[coverage_data$value == 0,
+    c("filename", "first_line", "last_line", "first_column", "last_column", "value")]
 }
 
 #' @export

commit 87c86e5bf5987ae949e3b7bab99d66a7a1824f80

BUG FIX: Commit efbc295 broke exclude().

R/data_frame.R
@@ -1,5 +1,5 @@
 #' @export
-as.data.frame.coverage <- function(x, row.names = NULL, optional = FALSE, ...) {
+as.data.frame.coverage <- function(x, row.names = NULL, optional = FALSE, sort = TRUE, ...) {
   re <-
     rex::rex(
       capture(name = "filename", something), ":",
@@ -16,7 +16,11 @@ as.data.frame.coverage <- function(x, row.names = NULL, optional = FALSE, ...) {
 
   df[] <- lapply(df, type.convert, as.is = TRUE)
   df$value <- unlist(x)
-  df <- df[order(df$filename, df$first_line, df$first_byte),]
+
+  if (sort) {
+    df <- df[order(df$filename, df$first_line, df$first_byte),]
+  }
+
   rownames(df) <- NULL
   df
 }

R/exclusions.R
@@ -1,5 +1,5 @@
 exclude <- function(coverage, exclusions = NULL, ...) {
-  df <- as.data.frame(coverage)
+  df <- as.data.frame(coverage, sort=FALSE)
 
   filenames <- unique(df$filename)
 
@@ -15,6 +15,8 @@ exclude <- function(coverage, exclusions = NULL, ...) {
 
   excl <- merge_exclusions(source_exclusions, exclusions)
 
+  excl <- excl[sapply(excl, FUN=length) > 0L]
+
   to_exclude <- vapply(seq_len(NROW(df)),
     function(i) {
       file <- df[i,"filename"]
@@ -22,8 +24,8 @@ exclude <- function(coverage, exclusions = NULL, ...) {
       all(seq(df[i,"first_line"], df[i, "last_line"]) %in% excl[[file]])
   }, logical(1))
 
-  if (any(to_exclude) == TRUE) {
-    coverage <- coverage[-as.numeric(sort(rownames(df)[to_exclude]))]
+  if (any(to_exclude)) {
+    coverage <- coverage[!to_exclude]
   }
 
   coverage
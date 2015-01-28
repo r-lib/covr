87c86e5bf5987ae949e3b7bab99d66a7a1824f80
hb <hb@aroma-project.org>
2015-01-27 19:42:36 -0800

    BUG FIX: Commit efbc295 broke exclude().

--- R/exclusions.R
+++ R/exclusions.R
@@ -1,5 +1,5 @@
 exclude <- function(coverage, exclusions = NULL, ...) {
-  df <- as.data.frame(coverage)
+  df <- as.data.frame(coverage, sort=FALSE)
 
   filenames <- unique(df$filename)
 
@@ -15,6 +15,8 @@ exclude <- function(coverage, exclusions = NULL, ...) {
 
   excl <- merge_exclusions(source_exclusions, exclusions)
 
+  excl <- excl[sapply(excl, FUN=length) > 0L]
+
   to_exclude <- vapply(seq_len(NROW(df)),
     function(i) {
       file <- df[i,"filename"]
@@ -22,8 +24,8 @@ exclude <- function(coverage, exclusions = NULL, ...) {
       all(seq(df[i,"first_line"], df[i, "last_line"]) %in% excl[[file]])
   }, logical(1))
 
-  if (any(to_exclude) == TRUE) {
-    coverage <- coverage[-as.numeric(sort(rownames(df)[to_exclude]))]
+  if (any(to_exclude)) {
+    coverage <- coverage[!to_exclude]
   }
 
   coverage
