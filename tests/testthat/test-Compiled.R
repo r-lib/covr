d59db3da3c07f479bdc47fb17786c53d5145d668
Jon Clayden <code@clayden.org>
2015-03-03 22:48:44 +0000

    Adding test for compiled code in a subdirectory of src

--- tests/testthat/test-Compiled.R
+++ tests/testthat/test-Compiled.R
@@ -12,3 +12,17 @@ test_that("Compiled code coverage is reported", {
 
   expect_equal(cov[cov$first_line == "22", "value"], 4)
 })
+
+test_that("Source code subdirectories are found", {
+  cov <- as.data.frame(package_coverage("TestCompiledSubdir", relative_path = TRUE))
+
+  expect_equal(cov[cov$first_line == "9", "value"], 4)
+
+  expect_equal(cov[cov$first_line == "15", "value"], 3)
+
+  expect_equal(cov[cov$first_line == "18", "value"], 0)
+
+  expect_equal(cov[cov$first_line == "20", "value"], 1)
+
+  expect_equal(cov[cov$first_line == "22", "value"], 4)
+})
