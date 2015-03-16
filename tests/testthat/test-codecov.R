b81744fc726f0284e143f9ff72e70f0e0c2a5059
Jim Hester <james.f.hester@gmail.com>
2015-03-16 11:18:30 -0400

    Tests for codecov/semaphore

--- tests/testthat/test-codecov.R
+++ tests/testthat/test-codecov.R
@@ -167,3 +167,31 @@ test_that("it works with circleci", {
     )
   )
 })
+test_that("it works with semaphore", {
+  devtools::with_envvar(c(
+    "CI" = "true",
+    "SEMAPHORE" = "true",
+    "BRANCH_NAME" = "master",
+    "SEMAPHORE_BUILD_NUMBER" = "5",
+    "SEMAPHORE_REPO_SLUG" = "tester/test",
+    "REVISION" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+    ),
+
+    with_mock(
+      `httr:::perform` = function(...) list(...),
+      `httr::content` = identity,
+      `httr:::body_config` = function(...) list(...),
+
+      res <- codecov("TestS4"),
+
+      url <- res[[4]]$url,
+
+      expect_match(url, "service=semaphore"),
+      expect_match(url, "branch=master"),
+      expect_match(url, "build=5"),
+      expect_match(url, "owner=tester"),
+      expect_match(url, "repo=test"),
+      expect_match(url, "commit=a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")
+    )
+  )
+})
