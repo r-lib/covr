1615429cee54ac36883370852415e95e1d1c90a5
Jim Hester <james.f.hester@gmail.com>
2015-03-16 13:31:08 -0400

    Set environment variables so tests work on travis

--- tests/testthat/test-codecov.R
+++ tests/testthat/test-codecov.R
@@ -1,42 +1,53 @@
 context("codecov")
 test_that("it generates a properly formatted json file", {
 
-  with_mock(
-    `httr:::perform` = function(...) list(...),
-    `httr::content` = identity,
-    `httr:::body_config` = function(...) list(...),
+  devtools::with_envvar(c(
+      "TRAVIS" = "false"
+      ),
+    with_mock(
+      `httr:::perform` = function(...) list(...),
+      `httr::content` = identity,
+      `httr:::body_config` = function(...) list(...),
 
-    res <- codecov("TestS4"),
-    json <- jsonlite::fromJSON(res[[5]][[1]]),
+      res <- codecov("TestS4"),
+      json <- jsonlite::fromJSON(res[[5]][[1]]),
 
-    expect_equal(json$files$name, "TestS4/R/TestS4.R"),
-    expect_equal(json$files$coverage[[1]],
-      c(NA, NA, NA, 5, 2, 5, 3, 5, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-        NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1)
-    ),
-    expect_equal(json$uploader, "R")
-  )
+      expect_equal(json$files$name, "TestS4/R/TestS4.R"),
+      expect_equal(json$files$coverage[[1]],
+        c(NA, NA, NA, 5, 2, 5, 3, 5, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+          NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1)
+        ),
+      expect_equal(json$uploader, "R")
+      )
+    )
 })
 
 test_that("it works with local repos", {
-  with_mock(
-    `httr:::perform` = function(...) list(...),
-    `httr::content` = identity,
-    `httr:::body_config` = function(...) list(...),
-    `local_branch` = function() "master",
-    `base::system` = function(...) "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3 ",
+  devtools::with_envvar(c(
+      "TRAVIS" = "false"
+    ),
+    with_mock(
+      `httr:::perform` = function(...) list(...),
+      `httr::content` = identity,
+      `httr:::body_config` = function(...) list(...),
+      `local_branch` = function() "master",
+      `base::system` = function(...) "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3 ",
 
-    res <- codecov("TestS4"),
+      res <- codecov("TestS4"),
 
-    url <- res[[4]]$url,
+      url <- res[[4]]$url,
 
-    expect_match(url, "/upload/v2"), # nolint
-    expect_match(url, "branch=master"),
-    expect_match(url, "commit=a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")
-  )
+      expect_match(url, "/upload/v2"), # nolint
+      expect_match(url, "branch=master"),
+      expect_match(url, "commit=a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")
+      )
+    )
 })
 test_that("it adds the token to the query if available", {
-  with_envvar(c("CODECOV_TOKEN" = "codecov_test"),
+  devtools::with_envvar(c(
+      "TRAVIS" = "false",
+      "CODECOV_TOKEN" = "codecov_test"
+    ),
     with_mock(
       `httr:::perform` = function(...) list(...),
       `httr::content` = identity,
@@ -53,16 +64,17 @@ test_that("it adds the token to the query if available", {
       expect_match(url, "commit=a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"),
       expect_match(url, "token=codecov_test")
       )
-  )
+    )
 })
 test_that("it works with jenkins", {
   devtools::with_envvar(c(
-    "JENKINS_URL" = "jenkins.com",
-    "GIT_BRANCH" = "test",
-    "GIT_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
-    "BUILD_NUMBER" = "1",
-    "BUILD_URL" = "http://test.com/tester/test"
-    ),
+      "TRAVIS" = "false",
+      "JENKINS_URL" = "jenkins.com",
+      "GIT_BRANCH" = "test",
+      "GIT_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
+      "BUILD_NUMBER" = "1",
+      "BUILD_URL" = "http://test.com/tester/test"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -84,15 +96,15 @@ test_that("it works with jenkins", {
 
 test_that("it works with travis normal builds", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "TRAVIS" = "true",
-    "TRAVIS_PULL_REQUEST" = "false",
-    "TRAVIS_REPO_SLUG" = "tester/test",
-    "TRAVIS_BRANCH" = "master",
-    "TRAVIS_JOB_NUMBER" = "100",
-    "TRAVIS_JOB_ID" = "10",
-    "TRAVIS_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "CI" = "true",
+      "TRAVIS" = "true",
+      "TRAVIS_PULL_REQUEST" = "false",
+      "TRAVIS_REPO_SLUG" = "tester/test",
+      "TRAVIS_BRANCH" = "master",
+      "TRAVIS_JOB_NUMBER" = "100",
+      "TRAVIS_JOB_ID" = "10",
+      "TRAVIS_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -117,15 +129,15 @@ test_that("it works with travis normal builds", {
 
 test_that("it works with travis pull requests", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "TRAVIS" = "true",
-    "TRAVIS_PULL_REQUEST" = "5",
-    "TRAVIS_REPO_SLUG" = "tester/test",
-    "TRAVIS_BRANCH" = "master",
-    "TRAVIS_JOB_NUMBER" = "100",
-    "TRAVIS_JOB_ID" = "10",
-    "TRAVIS_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "CI" = "true",
+      "TRAVIS" = "true",
+      "TRAVIS_PULL_REQUEST" = "5",
+      "TRAVIS_REPO_SLUG" = "tester/test",
+      "TRAVIS_BRANCH" = "master",
+      "TRAVIS_JOB_NUMBER" = "100",
+      "TRAVIS_JOB_ID" = "10",
+      "TRAVIS_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -150,13 +162,14 @@ test_that("it works with travis pull requests", {
 
 test_that("it works with codeship", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "CI_NAME" = "codeship",
-    "CI_BRANCH" = "master",
-    "CI_BUILD_NUMBER" = "5",
-    "CI_BUILD_URL" = "http://test.com/tester/test",
-    "CI_COMMIT_ID" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "TRAVIS" = "false",
+      "CI" = "true",
+      "CI_NAME" = "codeship",
+      "CI_BRANCH" = "master",
+      "CI_BUILD_NUMBER" = "5",
+      "CI_BUILD_URL" = "http://test.com/tester/test",
+      "CI_COMMIT_ID" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -177,14 +190,15 @@ test_that("it works with codeship", {
 })
 test_that("it works with circleci", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "CIRCLECI" = "true",
-    "CIRCLE_BRANCH" = "master",
-    "CIRCLE_BUILD_NUM" = "5",
-    "CIRCLE_PROJECT_USERNAME" = "tester",
-    "CIRCLE_PROJECT_REPONAME" = "test",
-    "CIRCLE_SHA1" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "TRAVIS" = "false",
+      "CI" = "true",
+      "CIRCLECI" = "true",
+      "CIRCLE_BRANCH" = "master",
+      "CIRCLE_BUILD_NUM" = "5",
+      "CIRCLE_PROJECT_USERNAME" = "tester",
+      "CIRCLE_PROJECT_REPONAME" = "test",
+      "CIRCLE_SHA1" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -206,13 +220,14 @@ test_that("it works with circleci", {
 })
 test_that("it works with semaphore", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "SEMAPHORE" = "true",
-    "BRANCH_NAME" = "master",
-    "SEMAPHORE_BUILD_NUMBER" = "5",
-    "SEMAPHORE_REPO_SLUG" = "tester/test",
-    "REVISION" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "TRAVIS" = "false",
+      "CI" = "true",
+      "SEMAPHORE" = "true",
+      "BRANCH_NAME" = "master",
+      "SEMAPHORE_BUILD_NUMBER" = "5",
+      "SEMAPHORE_REPO_SLUG" = "tester/test",
+      "REVISION" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -234,13 +249,14 @@ test_that("it works with semaphore", {
 })
 test_that("it works with drone", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "DRONE" = "true",
-    "DRONE_BRANCH" = "master",
-    "DRONE_BUILD_NUMBER" = "5",
-    "DRONE_BUILD_URL" = "http://test.com/tester/test",
-    "DRONE_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "TRAVIS" = "false",
+      "CI" = "true",
+      "DRONE" = "true",
+      "DRONE_BRANCH" = "master",
+      "DRONE_BUILD_NUMBER" = "5",
+      "DRONE_BUILD_URL" = "http://test.com/tester/test",
+      "DRONE_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -261,13 +277,14 @@ test_that("it works with drone", {
 })
 test_that("it works with AppVeyor", {
   devtools::with_envvar(c(
-    "CI" = "True",
-    "APPVEYOR" = "True",
-    "APPVEYOR_REPO_NAME" = "tester/test",
-    "APPVEYOR_REPO_BRANCH" = "master",
-    "APPVEYOR_BUILD_NUMBER" = "5",
-    "APPVEYOR_REPO_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "TRAVIS" = "false",
+      "CI" = "True",
+      "APPVEYOR" = "True",
+      "APPVEYOR_REPO_NAME" = "tester/test",
+      "APPVEYOR_REPO_BRANCH" = "master",
+      "APPVEYOR_BUILD_NUMBER" = "5",
+      "APPVEYOR_REPO_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
@@ -289,13 +306,14 @@ test_that("it works with AppVeyor", {
 })
 test_that("it works with Wercker", {
   devtools::with_envvar(c(
-    "CI" = "true",
-    "WERCKER_GIT_BRANCH" = "master",
-    "WERCKER_MAIN_PIPELINE_STARTED" = "5",
-    "WERCKER_GIT_OWNER" = "tester",
-    "WERCKER_GIT_REPOSITORY" = "test",
-    "WERCKER_GIT_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    ),
+      "TRAVIS" = "false",
+      "CI" = "true",
+      "WERCKER_GIT_BRANCH" = "master",
+      "WERCKER_MAIN_PIPELINE_STARTED" = "5",
+      "WERCKER_GIT_OWNER" = "tester",
+      "WERCKER_GIT_REPOSITORY" = "test",
+      "WERCKER_GIT_COMMIT" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+      ),
 
     with_mock(
       `httr:::perform` = function(...) list(...),
