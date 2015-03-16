849d085189f92f1d2c299558e794ddc7ed2e5f0c
Jim Hester <james.f.hester@gmail.com>
2015-03-16 11:16:22 -0400

    Add tests for codecov/circleci

--- tests/testthat/test-codecov.R
+++ tests/testthat/test-codecov.R
@@ -138,3 +138,32 @@ test_that("it works with codeship", {
     )
   )
 })
+test_that("it works with circleci", {
+  devtools::with_envvar(c(
+    "CI" = "true",
+    "CIRCLECI" = "true",
+    "CIRCLE_BRANCH" = "master",
+    "CIRCLE_BUILD_NUM" = "5",
+    "CIRCLE_PROJECT_USERNAME" = "tester",
+    "CIRCLE_PROJECT_REPONAME" = "test",
+    "CIRCLE_SHA1" = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+    ),
+
+    with_mock(
+      `httr:::perform` = function(...) list(...),
+      `httr::content` = identity,
+      `httr:::body_config` = function(...) list(...),
+
+      res <- codecov("TestS4"),
+
+      url <- res[[4]]$url,
+
+      expect_match(url, "service=circleci"),
+      expect_match(url, "branch=master"),
+      expect_match(url, "build=5"),
+      expect_match(url, "owner=tester"),
+      expect_match(url, "repo=test"),
+      expect_match(url, "commit=a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")
+    )
+  )
+})
