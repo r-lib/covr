2d2f40e59d3cda4cdedcbdce6459b6f1ccd94ed7
Jim Hester <james.f.hester@gmail.com>
2015-04-22 17:53:32 -0400

    Turn back on failing tests

--- tests/testthat/test-coveralls.R
+++ tests/testthat/test-coveralls.R
@@ -23,29 +23,29 @@ test_that("coveralls generates a properly formatted json file", {
   )
 })
 
-#test_that("coveralls can spawn a job using repo_token", {
+test_that("coveralls can spawn a job using repo_token", {
 
-  #with_envvar(c("CI_NAME" = "DRONE"),
-    #with_mock(
-      #`httr:::perform` = function(...) list(...),
-      #`httr::content` = identity,
-      #`httr::upload_file` = function(file) readChar(file, file.info(file)$size),
+  with_envvar(c("CI_NAME" = "DRONE"),
+    with_mock(
+      `httr:::perform` = function(...) list(...),
+      `httr::content` = identity,
+      `httr::upload_file` = function(file) readChar(file, file.info(file)$size),
 
-      #res <- coveralls("TestS4", repo_token="mytoken"),
-      #json <- jsonlite::fromJSON(res[[5]]$body$json_file),
+      res <- coveralls("TestS4", repo_token="mytoken"),
+      json <- jsonlite::fromJSON(res[[5]]$body$json_file),
 
-      #expect_equal(is.null(json$git), FALSE),
-      #expect_equal(nrow(json$source_files), 1),
-      #expect_equal(json$service_name, NULL),
-      #expect_equal(json$repo_token, "mytoken"),
-      #expect_equal(json$source_files$name, "R/TestS4.R"),
-      #expect_equal(json$source_files$source, read_file("TestS4/R/TestS4.R")),
-      #expect_equal(json$source_files$coverage[[1]],
-        #c(NA, NA, 5, 2, 5, 3, 5, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-          #NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1, NA))
-    #)
-  #)
-#})
+      expect_equal(is.null(json$git), FALSE),
+      expect_equal(nrow(json$source_files), 1),
+      expect_equal(json$service_name, NULL),
+      expect_equal(json$repo_token, "mytoken"),
+      expect_equal(json$source_files$name, "R/TestS4.R"),
+      expect_equal(json$source_files$source, read_file("TestS4/R/TestS4.R")),
+      expect_equal(json$source_files$coverage[[1]],
+        c(NA, NA, 5, 2, 5, 3, 5, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+          NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1, NA, NA, NA, NA, NA, 1, NA))
+    )
+  )
+})
 
 test_that("generates correct payload for Drone and Jenkins", {
 
