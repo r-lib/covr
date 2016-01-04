ced9642d95c612ec0d96fd25eba16baaff82eceb
Jim Hester <james.f.hester@gmail.com>
2016-01-04 08:48:28 -0500

    More portable shebangs

--- unshim_package.sh
+++ unshim_package.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/sh
+#!/usr/bin/env sh
 
 perl -i -pe 's/\bcovrShim\b/covr/g;s/\breassign_function_shim\b/reassign_function/g;s/\bduplicate_shim\b/duplicate_/g' DESCRIPTION NAMESPACE R/* src/* tests/*R tests/testthat/*R
