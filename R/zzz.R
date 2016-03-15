6a1392aad45973f22ce48d3781518074c36fa8f9
Jim Hester <james.f.hester@gmail.com>
2016-03-15 11:36:46 -0400

    Just use --coverage for all cases

--- R/zzz.R
+++ R/zzz.R
@@ -6,10 +6,10 @@
     covr.exclude_pattern = rex::rex("#", any_spaces, "nocov"),
     covr.exclude_start = rex::rex("#", any_spaces, "nocov", any_spaces, "start"),
     covr.exclude_end = rex::rex("#", any_spaces, "nocov", any_spaces, "end"),
-    covr.flags = c(CPPFLAGS = "-O0 -fprofile-arcs -ftest-coverage",
-                   FFLAGS = "-O0 -fprofile-arcs -ftest-coverage",
-                   FCFLAGS = "-O0 -fprofile-arcs -ftest-coverage",
-                   LDFLAGS = "-lgcov")
+  covr.flags = c(CPPFLAGS = "-g -O0 --coverage",
+                 FFLAGS = "-g -O0 --coverage",
+                 FCFLAGS = "-g -O0 --coverage",
+                 LDFLAGS = "--coverage")
   )
   toset <- !(names(op_covr) %in% names(op))
   if (any(toset)) options(op_covr[toset])
