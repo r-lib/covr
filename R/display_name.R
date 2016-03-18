63f6cba8eef79a4ce2c8a85c27775152df86f3aa
Jim Hester <james.f.hester@gmail.com>
2016-03-18 10:45:32 -0400

    Simply strip the package path for relative paths

--- R/display_name.R
+++ R/display_name.R
@@ -6,18 +6,8 @@ display_name <- function(x) {
 
   filenames <- vapply(x, function(x) getSrcFilename(x$srcref, full.names = TRUE), character(1))
   if (isTRUE(attr(x, "relative"))) {
-    strip_path_before(filenames, attr(x, "package")$package)
+    rex::re_substitutes(filenames, rex::rex(attr(x, "package")$path, "/"), "")
   } else {
     filenames
   }
-}
-
-# assumes all vectors has the same prefix you want to remove.
-strip_path_before <- function(x, before) {
-  x <- normalize_path(x)
-  d <- dirname(x[1])
-  while(basename(d) != before && d != ".") {
-    d <- dirname(d)
-  }
-  sub(paste0(d, "/"), "", x)
 }
