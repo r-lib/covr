ed76090d216b25ced84fe1d3f1fcf8a446876ef1
Jim Hester <james.f.hester@gmail.com>
2016-03-18 13:46:32 -0400

    Add tests for local_branch and current_commit

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -14,3 +14,12 @@ test_that("it returns the package if given the root or child directory", {
 
   expect_equal(as_package("TestS4/tests/testthat")$package, "TestS4")
 })
+
+context("local_branch")
+test_that("it works as expected", {
+  expect_equal(local_branch("TestSummary"), "master")
+}
+context("current_commit")
+test_that("it works as expected", {
+  expect_equal(current_commit("TestSummary"), "4b45ba2eb12b955a96c4783308345bf446292ce2")
+}
