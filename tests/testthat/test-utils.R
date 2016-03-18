92cb1006d056d686f35a12ecbcc5848c61efdf83
Jim Hester <james.f.hester@gmail.com>
2016-03-18 14:06:56 -0400

    Fix syntax error in utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -18,8 +18,9 @@ test_that("it returns the package if given the root or child directory", {
 context("local_branch")
 test_that("it works as expected", {
   expect_equal(local_branch("TestSummary"), "master")
-}
+})
+
 context("current_commit")
 test_that("it works as expected", {
   expect_equal(current_commit("TestSummary"), "4b45ba2eb12b955a96c4783308345bf446292ce2")
-}
+})
