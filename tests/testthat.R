1a6e642b20df738fc22fecb7c27b364990d78bba
Jim Hester <james.f.hester@gmail.com>
2016-03-21 14:19:05 -0400

    Skip tests on Solaris

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,11 @@ ops <- options("crayon.enabled" = FALSE, warn = 1)
 library(testthat)
 library("covr")
 
-Sys.setenv("R_TESTS" = "")
-test_check("covr")
+# Skip tests on Solaris as gcc is not in the PATH and I do not have an easy way
+# to mimic the CRAN build environment
+if (!tolower(Sys.info()[["sysname"]]) == "sunos") {
+  Sys.setenv("R_TESTS" = "")
+  test_check("covr")
+}
+
 options(ops)
