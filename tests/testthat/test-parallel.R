4f6ce44909ba0976d1a48ac4c45c664e776c2684
Karl Forner <karl.forner@quartz.bio>
2016-06-21 14:51:28 +0200

    skipped parallel test on windows (does not support fork/mcparallel)

--- tests/testthat/test-parallel.R
+++ tests/testthat/test-parallel.R
@@ -2,6 +2,7 @@ context("coverage of parallel code")
 
 
 test_that("mcparallel without the fix", {
+  skip_on_os("windows")
   cov <- package_coverage("TestParallel", type = "test")
   # only the non parallel code is covered
   expect_equal(floor(percent_coverage(cov)), 33)
@@ -9,8 +10,9 @@ test_that("mcparallel without the fix", {
 
 
 test_that("mcparallel with the fix", {
-    cov <- package_coverage("TestParallel", type = "test",
-      fix_parallel_mcexit = TRUE)
-    # only the non parallel code is covered
-    expect_equal(percent_coverage(cov), 100)
+  skip_on_os("windows")
+  cov <- package_coverage("TestParallel", type = "test",
+    fix_parallel_mcexit = TRUE)
+  # only the non parallel code is covered
+  expect_equal(percent_coverage(cov), 100)
 })
