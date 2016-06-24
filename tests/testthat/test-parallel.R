e72498893881d4c2fa183c4954ca743be01b91b1
Karl Forner <karl.forner@quartz.bio>
2016-06-24 17:14:45 +0200

    removed package_coverage() fix_parallel_mcexit param. Replaced it by autodetection (cf uses_parallel()), which is overridable using env var COVR.FIX_PARALLEL_MCEXIT or option covr.fix_parallel_mcexit

--- tests/testthat/test-parallel.R
+++ tests/testthat/test-parallel.R
@@ -3,16 +3,72 @@ context("coverage of parallel code")
 
 test_that("mcparallel without the fix", {
   skip_on_os("windows")
-  cov <- package_coverage("TestParallel", type = "test")
+
+  cov <- withr::with_options(list(covr.fix_parallel_mcexit = FALSE),
+    package_coverage("TestParallel", type = "test"))
   # only the non parallel code is covered
   expect_equal(floor(percent_coverage(cov)), 33)
 })
 
 
+
 test_that("mcparallel with the fix", {
   skip_on_os("windows")
-  cov <- package_coverage("TestParallel", type = "test",
-    fix_parallel_mcexit = TRUE)
+
+  # using auto detection
+  cov <- package_coverage("TestParallel", type = "test")
   # only the non parallel code is covered
   expect_equal(percent_coverage(cov), 100)
+})
+
+
+
+test_that("uses_parallel", {
+  pkg <- covr:::as_package("TestParallel")
+  expect_true(covr:::uses_parallel(pkg))
+
+  pkg <- covr:::as_package("TestSummary")
+  expect_false(covr:::uses_parallel(pkg))
+})
+
+
+
+test_that("should_enable_parallel_mcexit_fix", {
+  on.exit({
+      Sys.unsetenv('COVR.FIX_PARALLEL_MCEXIT')
+      options(covr.fix_parallel_mcexit = NULL)
+    }, add = TRUE
+  )
+
+  grid <- expand.grid(
+    var = c(NA, TRUE, FALSE),
+    option = c(NA, TRUE, FALSE),
+    pkg = c("TestParallel", "TestSummary"), stringsAsFactors = FALSE)
+
+  grid$res <- with(grid, ifelse(!is.na(var),
+      var,
+      ifelse(!is.na(option), option, pkg == "TestParallel")
+   ))
+
+  .test_config <- function(var, option, pkgname) {
+    if (is.na(var))
+      Sys.unsetenv('COVR.FIX_PARALLEL_MCEXIT')
+    else
+      Sys.setenv(COVR.FIX_PARALLEL_MCEXIT = var)
+
+
+    if (is.na(option))
+      options(covr.fix_parallel_mcexit = NULL)
+    else
+      options(covr.fix_parallel_mcexit = option)
+
+    pkg <- covr:::as_package(pkgname)
+    covr:::should_enable_parallel_mcexit_fix(pkg)
+  }
+
+  res <- with(grid, vapply(1:nrow(grid),
+      function(i) .test_config(var[i], option[i], pkg[i]), TRUE))
+
+  expect_identical(res, grid$res)
+
 })
