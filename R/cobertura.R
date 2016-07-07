5335400eb051dbc988b8dbb5ba914c839ee957f7
Jim Hester <james.f.hester@gmail.com>
2016-07-07 09:35:36 -0400

    Remove trailing whitespace and fix tests

--- R/cobertura.R
+++ R/cobertura.R
@@ -5,55 +5,55 @@
 #' @export
 to_cobertura <- function(cov, filename = 'cobertura.xml'){
   xmlDocument = XML::newXMLDoc()
-  
+
   df <- tally_coverage(cov, by = "line")
-  
-  top <- XML::newXMLNode(name = "coverage", 
+
+  top <- XML::newXMLNode(name = "coverage",
       attrs = c(
           "line-rate" = as.character(percent_coverage(df, by = "line")/100),
           "branch-rate" = "0",
           version = sessionInfo()$otherPkgs$covr$Version,
           timestamp = as.character(Sys.time())),
       parent = xmlDocument)
-  
+
   # Add sources
   sources <- XML::newXMLNode(
-      name = "sources", 
+      name = "sources",
       parent = top)
-  sourceFiles <- unique(covr::display_name(cov))
+  sourceFiles <- unique(display_name(cov))
   for(f in sourceFiles){
     source <- XML::newXMLNode(
-        name = "source", 
+        name = "source",
         text = f,
         parent = sources)
   }
-  
+
   # Add packages
   packages <- XML::newXMLNode(
-      name = "packages", 
+      name = "packages",
       parent = top)
-  
-  packageName <- attr(x, "package")$package
+
+  packageName <- attr(cov, "package")$package
   if(length(packageName) > 1){
     stop("We have covr results for more than one package? We do not support that for now.")
   }
   package <- XML::newXMLNode(
-      name = "package", 
+      name = "package",
       attrs = c(
           name = packageName,
           "line-rate" = as.character(percent_coverage(df, by = "line")/100),
           "branch-rate" = "0",
           complexity = "0"),
       parent = packages)
-  
+
   classes <- XML::newXMLNode(
-      name = "classes", 
+      name = "classes",
       parent = package)
-  
+
   # Add classes (for which we will use files for now)
   for(f in sourceFiles){
     cl <- XML::newXMLNode(
-        name = "class", 
+        name = "class",
         attrs = c(
             name = basename(f),
             filename = f,
@@ -63,11 +63,11 @@ to_cobertura <- function(cov, filename = 'cobertura.xml'){
         parent = classes)
     # Add methods
     ms <- XML::newXMLNode(
-        name = "methods", 
+        name = "methods",
         parent = cl)
     for(fname in unique(df$functions)){
       fun <- XML::newXMLNode(
-          name = "method", 
+          name = "method",
           attrs = c(
               name = fname,
               signature = "NA",
@@ -76,12 +76,12 @@ to_cobertura <- function(cov, filename = 'cobertura.xml'){
           parent = ms)
       # Add lines
       ls <- XML::newXMLNode(
-          name = "lines", 
+          name = "lines",
           parent = fun)
       for(i in seq_len(nrow(df[df$functions == fname, ]))){
         line <- df[df$functions == fname, ][i, ]
         l <- XML::newXMLNode(
-            name = "line", 
+            name = "line",
             attrs = c(
                 number = as.character(line$line),
                 hits = as.character(line$value),
@@ -91,12 +91,12 @@ to_cobertura <- function(cov, filename = 'cobertura.xml'){
     }
     # Add lines
     ls2 <- XML::newXMLNode(
-        name = "lines", 
+        name = "lines",
         parent = cl)
     for(i in seq_len(nrow(df))){
       line <- df[i, ]
       l <- XML::newXMLNode(
-          name = "line", 
+          name = "line",
           attrs = c(
               number = as.character(line$line),
               hits = as.character(line$value),
@@ -104,12 +104,12 @@ to_cobertura <- function(cov, filename = 'cobertura.xml'){
           parent = ls2)
     }
   }
-  
-  
+
+
   # This workaround was suggested by Duncan:
   # http://r.789695.n4.nabble.com/saveXML-prefix-argument-td4678407.html
-  
-  cat(XML::saveXML(xmlDocument, encoding = "UTF-8", indent = TRUE, prefix = cobertura_dtd()),
+
+  cat(XML::saveXML(xmlDocument, encoding = "UTF-8", indent = TRUE, prefix = cobertura_dtd()), "\n", sep = "",
       file = filename)
 }
 
@@ -119,7 +119,7 @@ to_cobertura <- function(cov, filename = 'cobertura.xml'){
 #' @author Willem Ligtenberg
 cobertura_dtd <- function(){
   return('<?xml version="1.0" encoding="UTF-8" ?>
-          
+
           <!-- Internal DTD -->
           <!DOCTYPE coverage [
           <!-- Portions (C) International Organization for Standardization 1986:
@@ -127,52 +127,52 @@ cobertura_dtd <- function(){
           conforming SGML systems and applications as defined in
           ISO 8879, provided this notice is included in all copies.
           -->
-          
+
           <!ELEMENT coverage (sources?,packages)>
           <!ATTLIST coverage line-rate   CDATA #REQUIRED>
           <!ATTLIST coverage branch-rate CDATA #REQUIRED>
           <!ATTLIST coverage version     CDATA #REQUIRED>
           <!ATTLIST coverage timestamp   CDATA #REQUIRED>
-          
+
           <!ELEMENT sources (source*)>
-          
+
           <!ELEMENT source (#PCDATA)>
-          
+
           <!ELEMENT packages (package*)>
-          
+
           <!ELEMENT package (classes)>
           <!ATTLIST package name        CDATA #REQUIRED>
           <!ATTLIST package line-rate   CDATA #REQUIRED>
           <!ATTLIST package branch-rate CDATA #REQUIRED>
           <!ATTLIST package complexity  CDATA #REQUIRED>
-          
+
           <!ELEMENT classes (class*)>
-          
+
           <!ELEMENT class (methods,lines)>
           <!ATTLIST class name        CDATA #REQUIRED>
           <!ATTLIST class filename    CDATA #REQUIRED>
           <!ATTLIST class line-rate   CDATA #REQUIRED>
           <!ATTLIST class branch-rate CDATA #REQUIRED>
           <!ATTLIST class complexity  CDATA #REQUIRED>
-          
+
           <!ELEMENT methods (method*)>
-          
+
           <!ELEMENT method (lines)>
           <!ATTLIST method name        CDATA #REQUIRED>
           <!ATTLIST method signature   CDATA #REQUIRED>
           <!ATTLIST method line-rate   CDATA #REQUIRED>
           <!ATTLIST method branch-rate CDATA #REQUIRED>
-          
+
           <!ELEMENT lines (line*)>
-          
+
           <!ELEMENT line (conditions*)>
           <!ATTLIST line number CDATA #REQUIRED>
           <!ATTLIST line hits   CDATA #REQUIRED>
           <!ATTLIST line branch CDATA "false">
           <!ATTLIST line condition-coverage CDATA "100%">
-          
+
           <!ELEMENT conditions (condition*)>
-          
+
           <!ELEMENT condition EMPTY>
           <!ATTLIST condition number CDATA #REQUIRED>
           <!ATTLIST condition type CDATA #REQUIRED>
