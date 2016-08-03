b64d54a995c4f415d8ce2a02aa659776fc265e4d
Jim Hester <james.f.hester@gmail.com>
2016-08-03 13:27:19 -0400

    Reduce test output for corner cases

--- tests/testthat/test-corner-cases.R
+++ tests/testthat/test-corner-cases.R
@@ -1,6 +1,9 @@
 context("corner-cases")
 
 test_that("corner-cases are handled as expected", {
-  cov <- file_coverage("corner-cases.R", "corner-cases-test.R")
+  expect_warning(withr::with_output_sink(tempfile(), {
+    cov <- file_coverage("corner-cases.R", "corner-cases-test.R")
+  }))
+
   expect_equal(as.data.frame(cov), readRDS("corner-cases.Rds"))
 })
