0e49bb678e9a909bb87cc85437a45b64893e3a24
Iñaki Úcar <i.ucar86@gmail.com>
2016-09-03 13:07:42 +0200

    run gcov per dir

--- R/compiled.R
+++ R/compiled.R
@@ -85,10 +85,15 @@ run_gcov <- function(path, quiet = TRUE,
   }
 
   gcov_inputs <- list.files(path, pattern = rex::rex(".gcno", end), recursive = TRUE, full.names = TRUE)
+  names(gcov_inputs) <- dirname(gcov_inputs)
+  gcov_inputs <- tapply(unlist(gcov_inputs, use.names=F), names(gcov_inputs), c)
   withr::with_dir(src_path, {
-    system_check(gcov_path,
-      args = c(gcov_args, gcov_inputs, "-o", dirname(gcov_inputs[[1]])),
-      quiet = quiet, echo = !quiet)
+    run_gcov <- function(src) {
+      system_check(gcov_path,
+        args = c(gcov_args, src, "-o", dirname(src[[1]])),
+        quiet = quiet, echo = !quiet)
+    }
+    lapply(gcov_inputs, run_gcov)
     gcov_outputs <- list.files(path, pattern = rex::rex(".gcov", end), recursive = TRUE, full.names = TRUE)
     structure(
       unlist(recursive = FALSE,
