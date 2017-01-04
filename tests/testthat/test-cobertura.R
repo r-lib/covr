22be9d0ba4311377e0c9e379d662dd5fc3e5c902
Jim Hester <james.f.hester@gmail.com>
2017-01-04 13:27:13 -0500

    Fix for failing test with soon to be released xml2 1.1.0

--- tests/testthat/test-cobertura.R
+++ tests/testthat/test-cobertura.R
@@ -1,9 +1,8 @@
 context("cobertura_export")
 
 test_that("it works with coverage objects", {
-  skip_on_cran()
   tmp <- tempfile()
   cov <- package_coverage("TestSummary")
   to_cobertura(cov, filename = tmp)
-  expect_equal(readLines(tmp)[-2], readLines("cobertura.xml")[-2])
+  expect_equal(readLines(tmp)[c(-1, -2)], readLines("cobertura.xml")[c(-1, -2)])
 })
