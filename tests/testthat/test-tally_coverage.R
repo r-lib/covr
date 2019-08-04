r-lib/covr

commit c3d77e68516838a871aa9902db783ba861cdcf62

Omit NA values in tally_coverage
These rarely occur when inputing source references in some cases. We may
still not track some of these lines, but at least we now not error on
them.

Fixes #322

R/summary_functions.R
@@ -18,7 +18,11 @@ percent_coverage <- function(x, ...) {
 #' @return a `data.frame` of coverage tallied by line or expression.
 #' @export
 tally_coverage <- function(x, by = c("line", "expression")) {
-  df <- as.data.frame(x)
+
+  # Rarely something goes wrong with the source references and we get all NAs
+  # for them, so we omit them here
+  df <- na.omit(as.data.frame(x))
+
   if (NROW(df) == 0) {
     return(df)
   }

commit 9511c39ced85c5f2349dc33beb6ea2a053d08ebf

Ensure compiled code is included in tally_coverage
This was a regression caused by https://github.com/r-lib/covr/commit/c3d77e68516838a871aa9902db783ba861cdcf62

Fixes #384

R/summary_functions.R
@@ -21,7 +21,10 @@ tally_coverage <- function(x, by = c("line", "expression")) {
 
   # Rarely something goes wrong with the source references and we get all NAs
   # for them, so we omit them here
-  df <- na.omit(as.data.frame(x))
+  df <- as.data.frame(x)
+
+  all_na_rows <- rowSums(is.na(df)) == ncol(df)
+  df <- df[!all_na_rows, ]
 
   if (NROW(df) == 0) {
     return(df)

tests/testthat/TestCompiled/NAMESPACE
@@ -1,3 +1,4 @@
 # Generated by roxygen2: do not edit by hand
 
 useDynLib(TestCompiled,simple_)
+useDynLib(TestCompiled,simple2_)

tests/testthat/TestCompiled/R/TestCompiled.R
@@ -4,3 +4,7 @@
 simple <- function(x) {
   .Call(simple_, x) # nolint
 }
+
+simple2 <- function(x) {
+  .Call(simple2_, x) # nolint
+}

tests/testthat/TestCompiled/tests/testthat/test-TestCompiled.R
@@ -5,3 +5,10 @@ test_that("compiled function simple works", {
   expect_equal(simple(3), 1)
   expect_equal(simple(-1), -1)
 })
+
+test_that("compiled function simple2 works", {
+  expect_equal(simple2(1), 1)
+  expect_equal(simple2(2), 1)
+  expect_equal(simple2(3), 1)
+  expect_equal(simple2(-1), -1)
+})

tests/testthat/test-tally_coverage.R
@@ -1,0 +1,20 @@
+test_that("tally_coverage includes compiled code", {
+  cov <- package_coverage(test_path("TestCompiled"))
+  tall <- tally_coverage(cov)
+
+  expect_named(tall, c("filename", "functions", "line", "value"))
+
+  expect_equal(
+    unique(tall$filename),
+    c("R/TestCompiled.R", "src/simple-header.h", "src/simple.c"))
+
+  expect_equal(
+    unname(as.list(tall[3, ])),
+    list("src/simple-header.h", NA_character_, 9L, 4L)
+  )
+
+  expect_equal(
+    unname(as.list(tall[15, ])),
+    list("src/simple.c", NA_character_, 10L, 4L)
+  )
+})