ca43cc2d9326fb9cba7b34a3a9c6d40d17fd3e3b
Jim Hester <james.f.hester@gmail.com>
2019-08-05 05:27:50 -0400

    Update cobertura output

--- tests/testthat/test-cobertura.R
+++ tests/testthat/test-cobertura.R
@@ -2,7 +2,7 @@ context("cobertura_export")
 
 test_that("it works with coverage objects", {
   tmp <- tempfile()
-  cov <- package_coverage("TestSummary")
+  cov <- package_coverage(test_path("TestSummary"))
   to_cobertura(cov, filename = tmp)
-  expect_equal(readLines(tmp)[c(-1, -2)], readLines("cobertura.xml")[c(-1, -2)])
+  expect_equal(readLines(tmp)[c(-1, -2)], readLines(test_path("cobertura.xml"))[c(-1, -2)])
 })
