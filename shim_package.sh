402e613aa83a017e01fa89efcbe9d47dc07901f1
Jim Hester <james.f.hester@gmail.com>
2019-08-09 06:50:58 -0400

    Fix shimming
    This was why the coverage was not being updated on codecov.io

--- shim_package.sh
+++ shim_package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-perl -i -pe 's/\bcovr\b/covrShim/g;s/\bcovr_/covrShim_/g' DESCRIPTION NAMESPACE R/* src/* tests/*R tests/testthat/*R
+perl -i -pe 's/\bcovr\b/covrShim/g;s/\bcovr_/covrShim_/g;s/_covr/_covrShim/g;' DESCRIPTION NAMESPACE R/* src/* tests/*R tests/testthat/*R
--- unshim_package.sh
+++ unshim_package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-perl -i -pe 's/\bcovrShim\b/covr/g;s/\bcovrShim_/covr_/g' DESCRIPTION NAMESPACE R/* src/* tests/*R tests/testthat/*R
+perl -i -pe 's/\bcovrShim\b/covr/g;s/\bcovrShim_/covr_/g;s/_covrShim/_covr/g;' DESCRIPTION NAMESPACE R/* src/* tests/*R tests/testthat/*R
