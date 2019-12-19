b117ea1fa0c32a738f399da3d605fa4ffbc0f02a
Filipe Valeriano <filipe.valeriano@talkdesk.com>
2019-12-19 10:27:43 +0000

    Added unit test for sonarqube export. Added expected report for sonarqube test.

---
+++ tests/testthat/test-sonarqube.R
@@ -0,0 +1,8 @@
+context("sonarqube_export")
+
+test_that("it works with coverage objects", {
+  tmp <- tempfile()
+  cov <- package_coverage(test_path("TestSummary"))
+  to_sonarqube(cov, filename = tmp)
+  expect_equal(readLines(tmp), readLines(test_path("sonarqube.xml")))
+})
