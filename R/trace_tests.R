3548313d1bcc74c910d453f06a555cf2dd6b362d
dgkf <18220321+dgkf@users.noreply.github.com>
2021-02-04 19:07:19 -0800

    adding handling for test files without source (custom test suites)

--- R/trace_tests.R
+++ R/trace_tests.R
@@ -74,6 +74,28 @@ count_test <- function(key) {
 
 #' Update current test if unit test expression has progressed
 #'
+#' Updating a test logs some metadata regarding the current call stack, noteably
+#' trying to capture information about the call stack prior to the covr::count
+#' call being traced. 
+#'
+#' There are a couple patterns of behavior, which try to accommodate a variety
+#' of testing suites:
+#'
+#' \itemize{
+#'   \item `testthat`: During execution of `testthat`'s `test_*` functions,
+#'     files are sourced and the working directory is temporarily changed to the
+#'     package `/tests` directory. Knowing this, calls in the call stack which
+#'     are relative to this directory are extracted and recorded. 
+#'   \item `RUnit`: 
+#'   \item `custom`: Any other custom test suites may not have source kept with
+#'     their execution, in which case the entire test call stack is kept. 
+#' }
+#'
+#'
+#' checks to see if the current call stack has the same
+#' `srcref` (or expression, if no source is available) at the same frame prior 
+#' to entering into a package where `covr:::count` is called. 
+#'
 #' @param key generated with [key()]
 #' @keywords internal
 #'
@@ -82,36 +104,47 @@ count_test <- function(key) {
 update_current_test <- function(key) {
   syscalls <- sys.calls()
   syscall_srcref_dirs <- lapply(syscalls, getSrcDirectory)
-  names(syscalls) <- syscall_srcref_dirs
   syscall_first_count <- Position(is_covr_count_call, syscalls)
 
   # find frames with relative srcref; ie src within /tests directory
   # this holds for `testthat`, `RUnit` test srcrefs are ambiguous
   has_srcdir <- vapply(syscall_srcref_dirs, length, integer(1L)) > 0L
+  srcdir_rel <- logical(length(has_srcdir))
+  srcdir_rel[has_srcdir] <- startsWith(as.character(syscall_srcref_dirs[has_srcdir]), ".")
   test_frames <- logical(length(syscall_srcref_dirs))
-  test_frames[has_srcdir] <- startsWith(as.character(syscall_srcref_dirs[has_srcdir]), ".")
-  test_frames <- which(test_frames)
+
+  if (any(srcdir_rel)) { 
+    # if tests are relative (likely testthat), take relative srcfiles
+    test_frames <- which(srcdir_rel)
+  } else {
+    # otherwise (likely RUnit, custom), capture call stack to trace counter
+    test_frames <- seq_len(syscall_first_count - 1L)
+  }
 
   # add in outer frame, which may call intermediate .Internal or .External
   exec_frames <- unique(c(test_frames, syscall_first_count - 1L))
 
-  # build data for current test and append to .counters$tests
+  # build updated current test data, isolating relevant frames
   .current_test$trace <- syscalls[exec_frames]
-  .current_test$frame <- exec_frames[[Position(
+  .current_test$frames <- exec_frames
+  .current_test$last_frame <- exec_frames[[Position(
     has_srcref, 
     .current_test$trace, 
     right = TRUE, 
     nomatch = length(exec_frames))]]
 
   # might be NULL if srcrefs aren't kept during building / sourcing
-  .current_test$srcref <- getSrcref(syscalls[[.current_test$frame]])
+  .current_test$src <- getSrcref(syscalls[[.current_test$last_frame]]) %||% 
+    syscalls[[.current_test$last_frame]]
 
   # build test data to store within .counters
   test <- list(.current_test$trace)
 
   # only name if srcrefs can be determined
-  if (!is.null(.current_test$srcref)) {
-    names(test) <- file.path(utils::getSrcDirectory(.current_test$srcref), key(.current_test$srcref))
+  if (inherits(.current_test$src, "srcref")) {
+    names(test) <- file.path(
+      utils::getSrcDirectory(.current_test$src), 
+      key(.current_test$src))
   }
 
   .counters$tests <- append(.counters$tests, test)
@@ -119,12 +152,25 @@ update_current_test <- function(key) {
 
 #' Returns TRUE if we've moved on from test reflected in .current_test
 #'
+#' Quickly dismiss the need to update the current test if we can. To test if
+#' we're still in the last test, check if the same srcref (or call, if source is
+#' not kept) exists at the last recorded calling frame prior to entering a covr
+#' trace. If this has changed, do a more comprehensive test to see if any of the
+#' test call stack has changed, in which case we are onto a new test. 
+#'
 is_current_test_finished <- function() {
   syscalls <- sys.calls()
 
-  is.null(.current_test$srcref) ||
-  .current_test$frame > length(syscalls) ||
-  !identical(.current_test$srcref, getSrcref(syscalls[[.current_test$frame]]))
+  is.null(.current_test$src) ||
+  .current_test$last_frame > length(syscalls) ||
+  !identical(
+    .current_test$src, 
+    getSrcref(syscalls[[.current_test$last_frame]]) %||% syscalls[[.current_test$last_frame]]
+   ) ||
+  !identical(
+    .current_test$trace,
+    syscalls[.current_test$frames]
+  )
 }
 
 has_srcref <- function(expr) {
