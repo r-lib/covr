337d1ccb34bce83aa5423c70a1ba6535a8458db1
Rodrigo Basa <rodrigo@appsilon.com>
2023-01-19 18:39:38 +0800

    now works with basic box modules

--- R/box.R
+++ R/box.R
@@ -1,3 +1,17 @@
 replacements_box <- function(env) {
-
+  unlist(recursive = FALSE, eapply(env, all.names = TRUE,
+      function(obj) {
+       if (inherits(obj, "box$mod")) {
+         lapply(names(obj),
+            function(f_name) {
+              f <- get(f_name, obj)
+              if (inherits(f, "function")) {
+                replacement(f_name, env = obj, target_value = f)
+              }
+            }
+          )
+        }
+      }
+    )
+  )
 }
--- tests/testthat/test-box.R
+++ tests/testthat/test-box.R
@@ -6,6 +6,9 @@ test_that("box module coverage is reported", {
         source_files = "app/app.R",
         test_files = list.files("tests/testthat", full.name = TRUE)))
 
+    expect_equal(cov$value, c(5, 2, 3))
+    expect_equal(cov$first_line, c(5, 6, 8))
+    expect_equal(cov$last_line, c(5, 6, 8))
     expect_true("a" %in% cov$functions)
   })
 
