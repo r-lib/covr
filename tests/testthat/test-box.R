9b9251ac9db06876bfa52e5c72330a446c23ebce
Rodrigo Basa <rodrigo@appsilon.com>
2023-01-20 18:36:50 +0800

    forgot to update the unit test with R6 modules attached

--- tests/testthat/test-box.R
+++ tests/testthat/test-box.R
@@ -6,11 +6,12 @@ test_that("box module coverage is reported", {
         source_files = "app/app.R",
         test_files = list.files("tests/testthat", full.names = TRUE)))
 
-    expect_equal(cov$value, c(5, 2, 3, 3))
-    expect_equal(cov$first_line, c(5, 6, 8, 13))
-    expect_equal(cov$last_line, c(5, 6, 8, 13))
+    expect_equal(cov$value, c(5, 2, 3, 3, 1, 1))
+    expect_equal(cov$first_line, c(5, 6, 8, 13, 5, 8))
+    expect_equal(cov$last_line, c(5, 6, 8, 13, 5, 8))
     expect_true("a" %in% cov$functions)
     expect_true("private_function" %in% cov$functions)
+    expect_true("show" %in% cov$functions)
   })
 
 })
