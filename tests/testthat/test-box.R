7d4a35a9266d3a9f28e6ab820ec96c9500e2964f
Rodrigo Basa <radbasa@yahoo.com>
2023-01-20 12:24:53 +0800

    Forgot an s
    Co-authored-by: Jakub Nowicki <q.nowicki@gmail.com>

--- tests/testthat/test-box.R
+++ tests/testthat/test-box.R
@@ -4,7 +4,7 @@ test_that("box module coverage is reported", {
   withr::with_dir("./Testbox", {
     cov <- as.data.frame(file_coverage(
         source_files = "app/app.R",
-        test_files = list.files("tests/testthat", full.name = TRUE)))
+        test_files = list.files("tests/testthat", full.names = TRUE)))
 
     expect_equal(cov$value, c(5, 2, 3))
     expect_equal(cov$first_line, c(5, 6, 8))
