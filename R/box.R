e932fe3821255a80de051da1ed6212f16382905b
Rodrigo Basa <rodrigo@appsilon.com>
2023-01-27 19:12:44 +0800

    a little bit of cleanup

--- R/box.R
+++ R/box.R
@@ -2,8 +2,8 @@ replacements_box_modules <- function(env) {
   unlist(recursive = FALSE, eapply(env, all.names = TRUE,
       function(obj) {
         if (inherits(attr(obj, "spec"), "box$mod_spec")) {
-         obj_impl <- attr(obj, "namespace")
-         lapply(ls(obj_impl),
+          obj_impl <- attr(obj, "namespace")
+          lapply(ls(obj_impl),
             function(f_name) {
               f <- get(f_name, obj_impl)
               if (inherits(f, "function")) {
