2a9ab9bb568833d5a13b338f89834b1fbf55811f
Rodrigo Basa <rodrigo@appsilon.com>
2023-01-30 18:13:23 +0800

    clean up nested ((

--- R/box.R
+++ R/box.R
@@ -3,26 +3,28 @@ replacements_box <- function(env) {
       function(obj) {
         if (inherits(attr(obj, "spec"), "box$mod_spec")) {
           obj_impl <- attr(obj, "namespace")
-          compact(c(
-            lapply(ls(obj_impl),
-              function(f_name) {
-                f <- get(f_name, obj_impl)
-                if (inherits(f, "function")) {
-                  replacement(f_name, env = obj, target_value = f)
-                }
-              }
-            ),
-            unlist(recursive = FALSE,
+          compact(
+            c(
               lapply(ls(obj_impl),
                 function(f_name) {
                   f <- get(f_name, obj_impl)
-                  if (inherits(f, "R6ClassGenerator")) {
-                    traverse_R6(f, obj)
+                  if (inherits(f, "function")) {
+                    replacement(f_name, env = obj, target_value = f)
                   }
                 }
+              ),
+              unlist(recursive = FALSE,
+                lapply(ls(obj_impl),
+                  function(f_name) {
+                    f <- get(f_name, obj_impl)
+                    if (inherits(f, "R6ClassGenerator")) {
+                      traverse_R6(f, obj)
+                    }
+                  }
+                )
               )
             )
-          ))
+          )
         }
       }
     )
