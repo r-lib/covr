4853eaa3f99bf5befed408b490a2eb2128fbb5f8
Jim Hester <jimhester@netflix.com>
2023-03-13 16:17:29 -0400

    Fix generic / method signature consistency
    This throws a warning in R devel

--- R/covr.R
+++ R/covr.R
@@ -594,15 +594,16 @@ merge_coverage <- function(x) {
 }
 
 #' @export
-merge_coverage.character <- function(files) {
-  coverage_objs <- lapply(files, function(f) {
+merge_coverage.character <- function(x) {
+  coverage_objs <- lapply(x, function(f) {
     as.list(suppressWarnings(readRDS(f)))
   })
   merge_coverage(coverage_objs)
 }
 
 #' @export
-merge_coverage.list <- function(coverage_objs) {
+merge_coverage.list <- function(x) {
+  coverage_obj <- x
   if (length(coverage_objs) == 0) {
     return()
   }
